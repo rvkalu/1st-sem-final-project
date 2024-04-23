8b97a6685e64dbca02d27b41b6004bb31790972c
rvkalu <rvkalupahana@gmail.com>
2024-04-23 13:09:02 +0530

    1st test

--- src/main/java/lk/ijse/dep/Main.java
+++ src/main/java/lk/ijse/dep/Main.java
@@ -1,7 +1,10 @@
 package lk.ijse.dep;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("rvkalu");
     }
 }
