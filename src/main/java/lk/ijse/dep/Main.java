5c3b4a0f12db53203766d2740fe69ae3e145a180
rvkalu <rvkalupahana@gmail.com>
2024-04-23 13:11:25 +0530

    3st test

--- src/main/java/lk/ijse/dep/Main.java
+++ src/main/java/lk/ijse/dep/Main.java
@@ -5,6 +5,6 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("rvkalu");
+        System.out.println("rvkalupahana");
     }
 }
